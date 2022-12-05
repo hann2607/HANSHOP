768b81c74ee144a311674b0ea28d6e70b8ab6b80
NguyenHan <handev2607@gmail.com>
2022-12-05 22:44:34 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -17,13 +17,13 @@ app.controller("signin-signup", function($scope, $window) {
 	// if login success, it will forward index.html 
 	$scope.login = function(index) {
 		if(index.SignInForm.$valid) {
-			$window.location.href = 'HANSHOP/index.html';
+			$window.location.href = '/index.html';
 		}
 	}
 
 	$scope.register = function(index) {
 		if(index.signUpForm.$valid) {
-			$window.location.href = 'HANSHOP/index.html';
+			$window.location.href = '/index.html';
 		}
 	}
 });
